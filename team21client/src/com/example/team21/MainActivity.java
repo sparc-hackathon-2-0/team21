c2e331293b99746b3fc3aaad97b251efb12e4079
Sean Salmon <phatcabbage@gmail.com>
2012-08-25 15:27:15 -0400

    implemented article fetching... still remains to be seen if it works

--- team21client/src/com/example/team21/MainActivity.java
+++ team21client/src/com/example/team21/MainActivity.java
@@ -6,7 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -27,9 +27,8 @@ public class MainActivity extends Activity {
 	String leftUrl = "http://www.google.com/";
 	String rightUrl = "";
 
-	
-	String leftid = "";
-	String rightid = "";
+	String leftId = "";
+	String rightId = "";
 
 	private String getServerUrl() {
 	  return getResources().getText(R.string.serverUrl).toString();
@@ -37,18 +36,37 @@ public class MainActivity extends Activity {
 	
 	public void next() {
 	  try {
-	    String[] sta = CRPC.getRPC(getServerUrl()).getRandomArticle();
-	    ImageButton choose = ((ImageButton) findViewById(R.id.lhsImage));
-	    choose.setImageURI(Uri.parse(sta[3]));
-	    leftUrl = sta[2];
-	    leftid = sta[0];
+	    Rpc rpc = CRPC.getRPC(getServerUrl());
+	    String[] left = rpc.getRandomArticle();
+	    String[] right = rpc.getRandomArticle();
+	    updateLeftArticle(left[2], left[0], left[3]);
+	    updateRightArticle(right[2], right[0], right[3]);
 	  } catch (Exception e) {
 	    shortAnnounce(e.getMessage());
 	    e.printStackTrace();
 	  }
-
 	}
 
+	private void updateLeftArticle(
+	    String articleUri,
+	    String articleId, 
+	    String imageUri) {
+	  this.leftUrl = articleUri;
+	  this.leftId = articleId;
+	  ImageView button = ((ImageView) findViewById(R.id.lhsImage));
+	  button.setImageURI(Uri.parse(imageUri));
+	}
+	
+	private void updateRightArticle(
+	    String articleUri,
+	    String articleId, 
+	    String imageUri) {
+	  this.rightUrl = articleUri;
+	  this.rightId = articleId;
+	  ImageView button = ((ImageView) findViewById(R.id.rhsImage));
+	  button.setImageURI(Uri.parse(imageUri));
+	}	    
+	
 	private void openUrl(String url) {
 	  Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 	  startActivity(webIntent);
@@ -56,7 +74,7 @@ public class MainActivity extends Activity {
 	
 	public void related(View view) {
 		try {
-      CRPC.getRPC(getServerUrl()).relateorunrelate(Long.parseLong(leftid), Long.parseLong(rightid), true);
+      CRPC.getRPC(getServerUrl()).relateorunrelate(Long.parseLong(leftId), Long.parseLong(rightId), true);
       next();
     } catch (NumberFormatException e) {
       shortAnnounce("The id's don't make sense. Go away.");
@@ -73,7 +91,7 @@ public class MainActivity extends Activity {
 
 	public void notrelated(View view) {
 		try {
-      CRPC.getRPC(getServerUrl()).relateorunrelate(Long.parseLong(leftid), Long.parseLong(rightid), true);
+      CRPC.getRPC(getServerUrl()).relateorunrelate(Long.parseLong(leftId), Long.parseLong(rightId), true);
       next();
 		} catch (NumberFormatException e) {
       shortAnnounce("The id's don't make sense. Go away.");
