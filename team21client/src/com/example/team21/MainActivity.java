a46ce08518c64d0c3fb1d3dcf37efc04a6ef117f
Sean Salmon <phatcabbage@gmail.com>
2012-08-25 16:16:50 -0400

    pulling down from the rpc now

--- team21client/src/com/example/team21/MainActivity.java
+++ team21client/src/com/example/team21/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.team21;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TableRow;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -18,6 +27,16 @@ public class MainActivity extends Activity {
 		next();
 	}
 
+	private void initView() {
+	  TableRow imageRow = (TableRow) findViewById(R.id.imageRow);
+	  int mainWidth = imageRow.getWidth();
+	  for (int id : (new int[]{ R.id.lhsImage, R.id.rhsImage})) {
+	    ImageView v = (ImageView) findViewById(id);
+	    v.setMaxWidth(mainWidth/2);
+	    v.setMinimumWidth(mainWidth/2);
+	  }
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
@@ -41,6 +60,7 @@ public class MainActivity extends Activity {
 	    String[] right = rpc.getRandomArticle();
 	    updateLeftArticle(left[2], left[0], left[3]);
 	    updateRightArticle(right[2], right[0], right[3]);
+	    //initView();
 	  } catch (Exception e) {
 	    shortAnnounce(e.getMessage());
 	    e.printStackTrace();
@@ -54,7 +74,8 @@ public class MainActivity extends Activity {
 	  this.leftUrl = articleUri;
 	  this.leftId = articleId;
 	  ImageView button = ((ImageView) findViewById(R.id.lhsImage));
-	  button.setImageURI(Uri.parse(imageUri));
+	  Bitmap image = getImageBitmap(imageUri);
+	  button.setImageBitmap(image);
 	}
 	
 	private void updateRightArticle(
@@ -64,7 +85,8 @@ public class MainActivity extends Activity {
 	  this.rightUrl = articleUri;
 	  this.rightId = articleId;
 	  ImageView button = ((ImageView) findViewById(R.id.rhsImage));
-	  button.setImageURI(Uri.parse(imageUri));
+	  Bitmap image = getImageBitmap(imageUri);
+	  button.setImageBitmap(image);
 	}	    
 	
 	private void openUrl(String url) {
@@ -109,4 +131,21 @@ public class MainActivity extends Activity {
 	private void shortAnnounce(String msg) {
 	  Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 	}
+	
+	private static Bitmap getImageBitmap(String url) {
+	  Bitmap bm = null;
+	  try {
+	    URL aURL = new URL(url);
+	    URLConnection conn = aURL.openConnection();
+	    conn.connect();
+	    InputStream is = conn.getInputStream();
+	    BufferedInputStream bis = new BufferedInputStream(is);
+	    bm = BitmapFactory.decodeStream(bis);
+	    bis.close();
+	    is.close();
+	  } catch (IOException e) {
+	    e.printStackTrace();
+	  }
+	  return bm;
+	} 
 }
